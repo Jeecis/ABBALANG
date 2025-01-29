13804caaf03f4372012db20af259845f4566492b
Jeecis <jekabs.cudars@zzdats.lv>
2025-01-29 22:29:48 +0200

    Finished HW

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+func yourself(victory interface{}) interface{} {
+	return victory
+}
+
+func main() {
+	// Variable declarations
+	someMusic := "a"
+	bjorn := 12345678
+	benny := bjorn
+	benny = 123 + 234
+	benny123 := "bjorn567"
+
+	// Loop
+	for benny > bjorn {
+		bjorn = bjorn + 1
+	}
+
+	// Boolean variables
+	dance := false
+	sing := false
+	life := false
+
+	// If-else chain
+	if dance == false {
+		dance = true
+	} else if sing == false {
+		sing = true
+	} else if sing == true {
+		sing = false
+	} else {
+		life = true
+	}
+
+	// Switch statement
+	var y interface{}
+	var val interface{}
+	switch y {
+	case 1:
+		val = 10
+	case 2:
+		val = 20
+	case 3:
+		val = 30
+	default:
+		val = 2
+	}
+
+	// Array declaration
+	val = []int{1, 2, 3}
+
+	// Prevent unused variable warnings
+	_, _, _, _, _, _, _ = someMusic, benny123, dance, sing, life, val, y
+}
